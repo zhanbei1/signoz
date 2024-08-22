70f62a3c09f1a31eb247bd7c18f54a9cb02da1c2
Yunus M <myounis.ar@live.com>
2024-08-23 00:07:12 +0530

    Revert "feat: tabs and filters for alert history page (#5655)"
    This reverts commit 5c82fc91ec8a17d6a8b66860871439aab2dc5a3b.

--- frontend/src/container/AlertHistory/index.tsx
+++
@@ -1,9 +0,0 @@
-function AlertHistory(): JSX.Element {
-	return (
-		<div>
-			<h1>Alert History</h1>
-		</div>
-	);
-}
-
-export default AlertHistory;
--- frontend/src/container/AlertHistory/types.ts
+++
@@ -1,4 +0,0 @@
-export enum AlertDetailsTab {
-	OVERVIEW = 'OVERVIEW',
-	HISTORY = 'HISTORY',
-}
